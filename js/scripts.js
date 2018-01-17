e614c4b91809f90218d24d32d89af845994c284f
Cameron Anderson and Kayla Ondracek <cameron@>
2018-01-17 09:27:50 -0800

    create transportation survey with two questions and display answers back to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
 $(document).ready(function() {
+  $("#transportation").submit(function(event) {
+    event.preventDefault();
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function() {
+      var workTransportationMode = $(this).val();
+      $("#work-responses").append(workTransportationMode + "<br>");
+    });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function() {
+      var funTransportationMode = $(this).val();
+      $("#fun-responses").append(funTransportationMode + "<br>");
+    });
+
+
+  });
 });
